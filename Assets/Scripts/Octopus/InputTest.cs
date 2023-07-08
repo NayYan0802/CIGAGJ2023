6198069765cc139e1264010d052e81d687a4277e
Coaris <283006482@qq.com>
2023-07-08 19:35:43 +0800

    1935

---
+++ Assets/Scripts/Octopus/InputTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTest : MonoBehaviour {
+
+        [Range(0, 5)]
+        public int tentacleIndex;
+        public Vector3 targetPosition;
+        void OnMouseDown() {
+                EventBus.Publish(new TentacleLoose(tentacleIndex));
+        }
+        void OnMouseUpAsButton() {
+                EventBus.Publish(new TentacleTouch(tentacleIndex, targetPosition));
+        }
+}
