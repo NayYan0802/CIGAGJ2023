ee44b9774d1d9e0b7c5450c6ac488fb0e01dddcd
NayYan0802 <103715225+NayYan0802@users.noreply.github.com>
2023-07-09 02:54:54 +0800

    tentacle debug

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -121,9 +121,10 @@ public class GameManager : MonoBehaviour
         {
             TPoint[i].Translate((Target[i] - (Vector2)TPoint[i].position).normalized*speed1*Time.deltaTime, Space.Self);
             tentacleList[i].position = TRoot[i];
-            tentacleList[i].up = (Vector2)TPoint[i].position - TRoot[i];
+            tentacleList[i].up = ((Vector2)TPoint[i].position - TRoot[i]).normalized;
+            tentacleList[i].Rotate(Vector3.back, TPoint[i].position.x < 0 ? 5:-5) ;
             Vector3 newscale = tentacleList[i].localScale;
-            newscale.y= Vector2.Distance((Vector2)TPoint[i].position, TRoot[i]) * 0.17f;
+            newscale.y= Vector2.Distance((Vector2)TPoint[i].position, TRoot[i]) * 0.2f;
             tentacleList[i].localScale = newscale;
         }
     }
